d1ea890a9152878be9f41fb273f592a60e0f3848
tae-tae05 <jinn.han05@gmail.com>
2024-11-06 12:36:36 -0700

    edited jsx files

---
+++ src/add_project/add_project.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Add_Project() {
+  return (
+    <main className='container-fluid bg-secondary text-center'>
+      <div>Add Project displayed here</div>
+    </main>
+  );
+}
---
+++ src/example_project/example_project.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Example_Project() {
+  return (
+    <main className='container-fluid bg-secondary text-center'>
+      <div>example project displayed here</div>
+    </main>
+  );
+}
