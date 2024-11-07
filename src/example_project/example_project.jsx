bdd2ffa3041cca1e6f6418fa31f4baae1f67222b
tae-tae05 <jinn.han05@gmail.com>
2024-11-07 16:16:23 -0700

    functionality of example project page

--- src/example_project/example_project.jsx
+++ src/example_project/example_project.jsx
@@ -1,48 +1,103 @@
 import React from 'react';
+import { useState } from 'react';
+import './example_project.css';
 
 export function Example_Project() {
   return (
     <main className='container-fluid text-center'>
       <h2>💠 Project Name 💠</h2>
 
-      <div class="project-info">
-          <span className="color1">Hook Size: </span>3.5mm
-          <label className="color1" for="count">Update </label>
-          <input className="color2" type="text" id="count" value="- mm" readonly />
-          <button className="button button1" type="submit">Submit</button>
+      {/* <div className="project-info">
+        <span className="color1">Hook Size: </span>3.5mm
+        <Update />
       </div>
 
       <div className="project-info">
-          <span className="color1">Yarn Type: </span>Acrylic yellow 5ply medium weight
-          <label className="color1" for="type">Update </label>
-          <input className="color2" type="text" id="type" value="input here" readonly />
-          <button className="button button1" type="submit">Submit</button>
+        <span className="color1">Yarn Type: </span>Acrylic yellow 5ply medium weight
+        <Update />
+      </div> */}
+
+      <div className="project-info">
+          <Counter_Button />
       </div>
 
       <div className="project-info">
-          Counter A: 0
-          <button className="button button1" type="button">Down</button>
-          <button className="button button1" type="button">Up</button>
-      </div>
-
-      <div className="project-info">
-          Counter B: 1
-          <button className="button button1" type="button">Down</button>
-          <button className="button button1" type="button">Up</button>
+          <Counter_Button />
       </div>
       
       <div className="project-info">
-          Counter C: 10
-          <button className="button button1" type="button">Down</button>
-          <button className="button button1" type="button">Up</button>
+          <Counter_Button />
       </div>
 
       <h2>Message History</h2>
-      <p className="messaging"><span className="color1">Jin: </span><span class="color2">What type of yarn should I use for a small amigurumi project?</span></p>
-      <p className="messaging"><span className="color1">Yuki: </span><span class="color2">Use cotton if it's your first project. Otherwise, chenille is a great choice.</span></p>
-      <p><label className="project-info" for="message">Message:</label></p>
-      <textarea id="message" name="message" rows="4" cols="50"></textarea>
-      <button className="button button1" type="Submit">Send</button>
+        <div>
+            <Chat />
+        </div>
+
   </main>
   );
+}
+
+function Chat() {
+    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState([]);
+
+    const handleInput = (event) => {
+        setMessage(event.target.value);
+    };
+
+    const sendMessage = () => {
+        if (message.trim() !== '') { //prevent sending empty messages
+            setMessages([...messages, message]);
+            setMessage(''); //clear input field after sending
+        }
+    };
+
+    return (
+        <div>
+            <ul>
+                {messages.map((msg, index) => (
+                    <li className="messaging" key={index}>{msg}</li>
+                ))}
+            </ul>
+            <input
+            type="text"
+            value={message}
+            onChange={handleInput}
+            palceholder="Type your message..."></input>
+            <button className="button button1" onClick={sendMessage}>Send</button>
+        </div>
+    )
+}
+
+// function Update() {
+//     return ( 
+//         <>
+//         Update: 
+//         <input className="color2" type="text" id="type" value="input here" readonly />
+//         <button className="button button1">Submit</button>
+//         </>
+//     )
+// }
+
+function Counter_Button() {
+    const [count, setCount] = useState(0);
+
+    const increment = () => {
+        setCount(count + 1);
+    };
+
+    const decrease = () => {
+        if(count > 0){
+            setCount(count - 1);
+        };
+    };
+
+    return (
+        <>
+            Count: {count}
+            <button className="button button1" onClick={increment}>Up</button>
+            <button className="button button1" onClick={decrease}>Down</button>
+        </>
+    )
 }
