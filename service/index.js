39fe552e3791e366ec6bec88e705aebc89c7ee2d
tae-tae05 <jinn.han05@gmail.com>
2024-11-14 15:58:59 -0700

    index.js

---
+++ service/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
+
+app.use(express.static('public'));
+
+app.get('*', (_req, res) => {
+  res.send({ msg: 'Simon service' });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
