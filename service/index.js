61bc42fc5ba6f8ac1d82762ee5df3bb46205f2ea
tae-tae05 <jinn.han05@gmail.com>
2024-11-25 13:39:54 -0700

    added service

--- service/index.js
+++ service/index.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const app = express();
+const uuid = require('uuid');
 
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
 app.use(express.static('public'));
 
+//parse json
+app.use(express.json());
+
+let users = {};
+let projects = {};
+
+// //router for service endpoints
+// var apiRouter = express.Router();
+// app.use(`/api`, apiRouter);
+
 app.get('*', (_req, res) => {
-  res.send({ msg: 'Simon service' });
+  res.send({ msg: 'Jin service' });
 });
 
 app.listen(port, () => {
