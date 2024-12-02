340b484fc496828f0266b36541326b43f2398666
tae-tae05 <jinn.han05@gmail.com>
2024-12-02 12:30:27 -0700

    database

--- service/database.js
+++ service/database.js
@@ -3,5 +3,9 @@ const bcrypt = require('bcrypt');
 const uuid = require('uuid');
 const config = require('./dbConfig.json');
 
+const db = client.db('jin');
+const userCollection = db.collection('user');
+const projectCollection = db.collection('project');
+
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
