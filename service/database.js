d4f429beb78963e03c6b7cc253d84b5e9d02f55a
tae-tae05 <jinn.han05@gmail.com>
2024-12-03 17:57:43 -0700

    database config

--- service/database.js
+++ service/database.js
@@ -3,9 +3,46 @@ const bcrypt = require('bcrypt');
 const uuid = require('uuid');
 const config = require('./dbConfig.json');
 
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+
 const db = client.db('jin');
 const userCollection = db.collection('user');
 const projectCollection = db.collection('project');
 
-const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
-const client = new MongoClient(url);
+(async function testConnection() {
+    await client.connect();
+    await db.command({ ping: 1 });
+  })().catch((ex) => {
+    console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+    process.exit(1);
+  });
+
+async function createUser(email, password) {
+    // Hash the password before we insert it into the database
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    const user = {
+        email: email,
+        password: passwordHash,
+        token: uuid.v4(),
+    };
+    await userCollection.insertOne(user);
+
+
+    return user;
+}
+
+function getUser(email) {
+    return userCollection.findOne({ email: email });
+  }
+  
+  function getUserByToken(token) {
+    return userCollection.findOne({ token: token });
+  }
+
+  module.exports = {
+    getUser,
+    getUserByToken,
+    createUser,
+  };
