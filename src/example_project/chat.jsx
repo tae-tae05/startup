236aaf774f9955b5dccc6c4954dd9868c9eb3881
tae-tae05 <jinn.han05@gmail.com>
2024-12-10 13:27:51 -0700

    more clean up

---
+++ src/example_project/chat.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useState } from 'react';
+import './example_project.css';
+
+export function Chat() {
+    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState([]);
+
+    const handleInput = (event) => {
+        setMessage(event.target.value);
+    };
+
+    const sendMessage = () => {
+        if (message.trim() !== '') { //prevent sending empty messages
+            setMessages([...messages, message]);
+            setMessage(''); //clear input field after sending
+        }
+    };
+
+    return (
+        <div>
+            <ul>
+                {messages.map((msg, index) => (
+                    <li className="messaging" key={index}>{msg}</li>
+                ))}
+            </ul>
+            <input
+            type="text"
+            value={message}
+            onChange={handleInput}
+            ></input>
+            <button className="button button1" onClick={sendMessage}>Send</button>
+        </div>
+    )
+}
--- src/example_project/example_project.jsx
+++ src/example_project/example_project.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import './example_project.css';
 import { Fact } from './project_info';
 import { useParams } from 'react-router-dom';
+import { Chat } from './chat';
 
 
 export function Example_Project(props) {
@@ -139,37 +140,5 @@ function Counter() {
             <button className="button button1" onClick={() => increment(3)}>Up</button>
             <button className="button button1" onClick={() => decrease(3)}>Down</button>
         </>
-    )
-}
-
-function Chat() {
-    const [message, setMessage] = useState('');
-    const [messages, setMessages] = useState([]);
-
-    const handleInput = (event) => {
-        setMessage(event.target.value);
-    };
-
-    const sendMessage = () => {
-        if (message.trim() !== '') { //prevent sending empty messages
-            setMessages([...messages, message]);
-            setMessage(''); //clear input field after sending
-        }
-    };
-
-    return (
-        <div>
-            <ul>
-                {messages.map((msg, index) => (
-                    <li className="messaging" key={index}>{msg}</li>
-                ))}
-            </ul>
-            <input
-            type="text"
-            value={message}
-            onChange={handleInput}
-            ></input>
-            <button className="button button1" onClick={sendMessage}>Send</button>
-        </div>
     )
 }
