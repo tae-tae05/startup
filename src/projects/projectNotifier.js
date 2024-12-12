bd520f7432cdd66296f1eb2e1d7e15003ff231b9
tae-tae05 <jinn.han05@gmail.com>
2024-12-11 17:20:52 -0700

    working on broadcast

---
+++ src/projects/projectNotifier.js
@@ -0,0 +1,62 @@
+const ProjectEvent = {
+    System: 'system',
+    Submit: 'newProject',
+  };
+  
+  class EventMessage {
+    constructor(from, type, value) {
+      this.from = from;
+      this.type = type;
+      this.value = value;
+    }
+  }
+
+
+class ProjectEventNotifier {
+    events = [];
+    handlers = [];
+  
+    constructor() {
+      let port = window.location.port;
+      const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+      this.socket = new WebSocket(`${protocol}://${window.location.hostname}:${port}/ws`);
+      this.socket.onopen = (event) => {
+        this.receiveEvent(new EventMessage('Jin', ProjectEvent.System, { msg: 'connected' }));
+      };
+      this.socket.onclose = (event) => {
+        this.receiveEvent(new EventMessage('Jin', ProjectEvent.System, { msg: 'disconnected' }));
+      };
+      this.socket.onmessage = async (msg) => {
+        try {
+          const event = JSON.parse(await msg.data.text());
+          this.receiveEvent(event);
+        } catch {}
+      };
+    }
+  
+    broadcastEvent(from, type, value) {
+      const event = new EventMessage(from, type, value);
+      this.socket.send(JSON.stringify(event));
+    }
+  
+    addHandler(handler) {
+      this.handlers.push(handler);
+    }
+  
+    removeHandler(handler) {
+      this.handlers.filter((h) => h !== handler);
+    }
+  
+    receiveEvent(event) {
+      this.events.push(event);
+  
+      this.events.forEach((e) => {
+        this.handlers.forEach((handler) => {
+          handler(e);
+        });
+      });
+    }
+  }
+  
+  const ProjectNotifier = new ProjectEventNotifier();
+  export { ProjectEvent, ProjectNotifier };
--- src/projects/projects.jsx
+++ src/projects/projects.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './projects.css'
 import { Link } from 'react-router-dom';
+import { ProjectEvent, ProjectNotifier } from './projectNotifier';
 
 export function Projects() {
   const [projects, setProjects] = React.useState([]);
